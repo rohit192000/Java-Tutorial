24c51e30f39e7a3e443f8b2933b56988c0ab212b
rohit192000 <rohitsamal2000@gmail.com>
2022-10-21 15:06:58 +0530

    Updates

--- Arrays/ArraysBasicStructure.java
+++ Arrays/ArraysBasicStructure.java
@@ -1,9 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-public class ArrayBasicStructure
+public class ArraysBasicStructure
 {
     public static void main(String[] args){
-        
+        // Declaring an array 
+        int[] a;
+        // Initializing an array of size 5
+        a = new int[5];
+        // Insert values into array
+        a[0] = 1;
+        a[1] = 1;
+        a[2] = 1;
+        a[3] = 1;
+        a[4] = 1;
+        // Print the array using for each loop
+        for(int i:a){
+            System.out.println(i);
+        }
     }
 }
