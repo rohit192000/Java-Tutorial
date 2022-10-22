e8e3dd68820aac9d7577aafd88700441d9c83a25
rohit192000 <rohitsamal2000@gmail.com>
2022-10-22 18:19:50 +0530

    Array of Objects literal

--- Arrays/ArrayOfObjects.java
+++ Arrays/ArrayOfObjects.java
@@ -9,6 +9,13 @@ class Student{
         this.a = a;
         this.b = b;
     }
+    Student(String b){
+        this.b = b;
+    }
+    @Override //Uncomment fuction to print string in 2nd loop
+    public String toString(){
+        return b; //Converting string objects 
+    }
 }
 
 public class ArrayOfObjects
@@ -16,7 +23,8 @@ public class ArrayOfObjects
     public static void main(String[] args)
     {
         Student[] std = new Student[5];
-
+        Student[] myStudents = new Student[]{new Student("Dharma"),new Student("sanvi"),new Student("Rupa"),new Student("Ajay")};
+         // Array of 5 students created but No students are there in the array
         // Initializing array elements with the reference to objects of Student Class
         std[0] = new Student(1, "Ross");
         std[1] = new Student(2, "Joey");
@@ -28,5 +36,10 @@ public class ArrayOfObjects
         for(int i = 0; i<std.length; ++i){
             System.out.println(std[i].a+" "+std[i].b);
         }
+        // It will print the address of the objects
+        for(Student i:myStudents){
+            System.out.println(i);
+        }
+
     }
 }
